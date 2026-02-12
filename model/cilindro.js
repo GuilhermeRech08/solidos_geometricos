07b4892caae0a9c3986a63bf904c48bd70fcd5b6
GuilhermeRech08 <guilherme_s_rech@estudante.sesisenai.org.br>
2026-02-12 09:00:56 -0300

    criado as classes cilindro e cone

---
+++ model/cilindro.js
@@ -0,0 +1,23 @@
+class Cilindro{
+    constructor(raio, altura){
+        this.raio = raio
+        this.altura = altura
+    }
+
+    calcularArea(){
+        // Area da base = PI . r²
+        let area = 0.0
+        area = Math.PI * this.raio * this.raio
+        return area
+    }
+
+    calcularVolume(){
+        // Area da base = PI . r²
+        // Volume = Area da base . h
+        let volume = 0.0
+        volume = this.calcularArea() * this.altura
+        return volume
+    }
+}
+
+module.exports = Cilindro
